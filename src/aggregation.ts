739aeed8fb28631346f604caf5f5270f574a0d92
bear421 <exabear@gmail.com>
2025-06-30 10:07:37 +0200

    removed unsupported trade aggregations

--- src/aggregation.ts
+++ src/aggregation.ts
@@ -34,14 +34,6 @@ export interface MutableLogAggregation {
      */
     totalTrades: number;
     /**
-     * total number of items bought from players, highly inaccurate because there's no disambiguation between NPC/player tradeAccepted events
-     */
-    totalItemsBought: number;   
-    /**
-     * total number of items sold to players, highly inaccurate because there's no disambiguation between NPC/player tradeAccepted events
-     */
-    totalItemsSold: number;
-    /**
      * total number of buys attempted from players. based on whispers sent
      */
     totalBuysAttempted: number;
@@ -105,9 +97,11 @@ export const metricMeta: Record<Metric, MetricMeta> = {
 
 export enum Aggregation {
     total,
-    average,
     median,
     exactMedian,
+    average,
+    max,
+    min,
 }
 
 export interface CharacterInfo {
@@ -306,21 +300,17 @@ function withFilteredCharacters(ca: CharacterAggregation, filter: Filter): Chara
     return new CharacterAggregation(ca.characterLevelIndex, ca.characterTsIndex, ca.characterLevelSegmentation, ca.characters, filteredCharacters);
 }
 
-const MAX_SALE_OFFSET_MILLIS = 1000 * 60 * 10;
 const SESSION_THRESHOLD_MILLIS = 1000 * 60 * 60;
 const TRADE_PATTERN = /^(Hi, I would like to buy your|你好，我想購買|안녕하세요, |こんにちは、 |Здравствуйте, хочу купить у вас |Hi, ich möchte |Bonjour, je souhaiterais t'acheter )/;
 
 function aggregate0(maps: MapInstance[], events: LogEvent[], filter: Filter, characterAggregation: CharacterAggregation): MutableLogAggregation {
-    let totalBuys = 0, totalSales = 0, totalBuysAttempted = 0, totalSalesAttempted = 0, totalTrades = 0;
+    let totalBuysAttempted = 0, totalSalesAttempted = 0, totalTrades = 0;
     let totalDeaths = 0, totalWitnessedDeaths = 0;
-    const recentSales: AnyCharacterEvent[] = [];
-    const recentBuys: AnyCharacterEvent[] = [];
-    const probableNearbyCharacters = new Map<string, AnyCharacterEvent>();
     const messages = new Map<string, AnyMsgEvent[]>();
     let totalBossKills = 0;
     let totalSessions = 1;
     let prevEvent: LogEvent | null = null;
-    eventLoop: for (let i = 0; i < events.length; i++) {
+    for (let i = 0; i < events.length; i++) {
         const event = events[i];
         if (prevEvent) {
             const tsDelta = event.ts - prevEvent.ts;
@@ -333,77 +323,17 @@ function aggregate0(maps: MapInstance[], events: LogEvent[], filter: Filter, cha
             case "msgFrom":
                 if (TRADE_PATTERN.test(event.detail.msg)) {
                     totalSalesAttempted++;
-                    recentSales.push(event);
                 }
                 computeIfAbsent(messages, event.detail.character, () => []).push(event);
                 break;
             case "msgTo":
                 if (TRADE_PATTERN.test(event.detail.msg)) {
                     totalBuysAttempted++;
-                    recentBuys.push(event);
                 }
                 computeIfAbsent(messages, event.detail.character, () => []).push(event);
                 break;
             case "tradeAccepted":
                 totalTrades++;
-                /*
-                    proper trade attribution is impossible without additional user input such as from 3rd party trade tools. here's why:
-                    - NPC tradeAccepted events are indistinguishable from player tradeAccepted events
-                    - it is not explicitly logged who or what tradeAccepted refers to
-                    - whispers to and from Korean users are excluded from the log file (https://www.pathofexile.com/forum/view-thread/2567280/page/4)
-                    - it is impossible to accurately track characters in the current instance;
-                        when joining an instance with character(s) already present, the client doesn't generate a log of the present character(s) for the joiner
-                */
-                const thresholdTs = event.ts - MAX_SALE_OFFSET_MILLIS;
-                const discardStaleEvents = (events: AnyCharacterEvent[]) => {
-                    const index = binarySearch(events, thresholdTs, (e) => e.ts, BinarySearchMode.LAST);
-                    if (index !== -1) {
-                        events.splice(0, index);
-                    }
-                }
-                discardStaleEvents(recentSales);
-                discardStaleEvents(recentBuys);
-                const expectSale = !!recentSales.length;
-                const expectBuy = !!recentBuys.length;
-                if (!expectSale && !expectBuy) continue eventLoop;
-
-                // high confidence attributions (still inaccurate)
-                if (probableNearbyCharacters.size) {
-                    if (expectSale) {
-                        for (let i = 0; i < recentSales.length; i++) {
-                            const sale = recentSales[i];
-                            if (probableNearbyCharacters.has(sale.detail.character)) {
-                                totalSales++;
-                                recentSales.splice(i, 1);
-                                continue eventLoop;
-                            }
-                        }
-                    }
-                    if (expectBuy) {
-                        for (let i = 0; i < recentBuys.length; i++) {
-                            const buy = recentBuys[i];
-                            if (probableNearbyCharacters.has(buy.detail.character)) {
-                                totalBuys++;
-                                recentBuys.splice(i, 1);
-                                continue eventLoop;
-                            }
-                        }
-                    }
-                }
-                // low confidence attributions
-                if (expectSale) {
-                    if (expectBuy && recentBuys[recentBuys.length - 1].ts > recentSales[recentSales.length - 1].ts) {
-                        recentBuys.pop();
-                        totalBuys++;
-                    } else {
-                        recentSales.pop();
-                        totalSales++;
-                    }
-                } else { // necessarily implies expectBuy
-                    recentBuys.pop();
-                    totalBuys++;
-                }
-                // while many other heuristics such as load-times and hideout names exist, they likely wouldn't contribute much to accuracy
                 break;
             case "death":
                 if (filter.fromAreaLevel && event.detail.areaLevel < filter.fromAreaLevel) break;
@@ -416,15 +346,6 @@ function aggregate0(maps: MapInstance[], events: LogEvent[], filter: Filter, cha
                     totalWitnessedDeaths++;
                 }
                 break;
-            case "joinedArea":
-                probableNearbyCharacters.set(event.detail.character, event);
-                break;
-            case "leftArea":
-                probableNearbyCharacters.delete(event.detail.character);
-                break;
-            case "areaPostLoad":
-                probableNearbyCharacters.clear();
-                break;
             case "bossKill":
                 // assume pinacle bosses are at least i75+ otherwise this matches campaign bosses such as King in the Mists
                 if (event.detail.areaLevel >= 75) {
@@ -434,14 +355,20 @@ function aggregate0(maps: MapInstance[], events: LogEvent[], filter: Filter, cha
         }
     }
 
-    let totalMapTime = 0, totalLoadTime = 0, totalHideoutTime = 0;
+    let totalMapTime = 0, totalLoadTime = 0, totalHideoutTime = 0
+    const mapsUnique: MapInstance[] = [];
+    const mapsDelve: MapInstance[] = [];
     for (const map of maps) {
         totalMapTime += MapSpan.mapTime(map.span);
         totalLoadTime += map.span.loadTime;
         totalHideoutTime += map.span.hideoutTime;
+        if (map.isUnique) {
+            mapsUnique.push(map);
+        }
+        if (map.areaType === AreaType.Delve) {
+            mapsDelve.push(map);
+        }
     }
-    const mapsUnique = maps.filter(m => m.isUnique);
-    const mapsDelve = maps.filter(m => m.areaType === AreaType.Delve);
     return {
         maps,
         mapsUnique,
@@ -449,8 +376,6 @@ function aggregate0(maps: MapInstance[], events: LogEvent[], filter: Filter, cha
         events,
         messages,
         totalTrades,
-        totalItemsBought: totalBuys,
-        totalItemsSold: totalSales,
         totalBuysAttempted,
         totalSalesAttempted,
         totalDeaths,
@@ -1154,6 +1079,9 @@ function aggregateBy0(agg: LogAggregation, dimension: Dimension, metric: Metric,
     const aggregatedData = new Map<string | number, number>();
     for (const [key, values] of dataMap.entries()) {
         switch(aggregation) {
+            case Aggregation.total:
+                aggregatedData.set(key, total(values));
+                break;
             case Aggregation.average:
                 aggregatedData.set(key, average(values));
                 break;
@@ -1163,8 +1091,11 @@ function aggregateBy0(agg: LogAggregation, dimension: Dimension, metric: Metric,
             case Aggregation.exactMedian:
                 aggregatedData.set(key, values.length === 0 ? 0 : medianExact(values));
                 break;
-            case Aggregation.total:
-                aggregatedData.set(key, total(values));
+            case Aggregation.max:
+                aggregatedData.set(key, values.length === 0 ? 0 : max(values));
+                break;
+            case Aggregation.min:
+                aggregatedData.set(key, values.length === 0 ? 0 : min(values));
                 break;
         }
     }
@@ -1244,6 +1175,14 @@ export function average(arr: number[]): number {
 
 export function total(arr: number[]): number {
     return arr.reduce((acc, val) => acc + val, 0);
+}
+
+export function max(arr: number[]): number {
+    return arr.reduce((acc, val) => Math.max(acc, val), -Infinity);
+}
+
+export function min(arr: number[]): number {
+    return arr.reduce((acc, val) => Math.min(acc, val), Infinity);
 }
 
 function computeIfAbsent<K, V>(map: Map<K, V>, key: K, compute: () => V): V {
